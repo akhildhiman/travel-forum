1b944421f8ecc92f4a4f94596ff6ec2a066d49cf
akhildhiman <akhilblackmetal69@gmail.com>
2020-06-24 13:20:39 +0530

    get user profile from google

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,5 +1,6 @@
 const passport = require("passport")
 const GoogleStrategy = require("passport-google-oauth20").Strategy
+const User = require("../models/User")
 
 passport.use(
   new GoogleStrategy(
@@ -12,6 +13,13 @@ passport.use(
       // passport callback function
       console.log("passport callback function fired")
       console.log(profile)
+      // creating a new user
+      new User({
+        username: profile.displayName,
+        googleID: profile.id
+      }).save().then((newUser) => {
+        console.log(`New user created- ${newUser}`)
+      })
     }
   )
 )
--- models/User.js
+++ models/User.js
@@ -4,9 +4,10 @@ const Schema = mongoose.Schema
 const userSchema = new Schema(
   {
     username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    country: { type: String, required: true },
+    googleID: { type: String, required: true },
+    // email: { type: String, required: true },
+    // password: { type: String, required: true },
+    country: { type: String },
     friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
     posts: [{ types: Schema.Types.ObjectId, ref: "Post" }],
   },
